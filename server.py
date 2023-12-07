4804873a078203c228345edafe0fe5c819b4c70e
AustinHCopley <acopley@mines.edu>
2023-12-06 20:17:37 -0500

    added simple querying with prepared statements to server

--- server.py
+++ server.py
@@ -8,6 +8,27 @@ class Server:
             password="pwrd",
             database="patient_tracker"
         )
+        self.cursor = self.db.cursor()
+
+    def login(self, email, pw):
+        result = self.query("SELECT * FROM doctor")
+        print("received", len(result))
+        return True
+
+    def query(self, stmnt, vals=None):
+        try:
+            if not self.db.is_connected():
+                self.db.reconnect()
+
+            cursor = self.db.cursor()
+            cursor.execute(stmnt, vals)
+            result = cursor.fetchall()
+            cursor.close()
+            return result
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
 
 # if __name__ == "__main__":
