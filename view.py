176b979dd949b15a7cae78c1f0c6876b40d3f593
AustinHCopley <acopley@mines.edu>
2023-12-06 20:14:00 -0500

    created abstract Page class, implemented child classes and basic Login screen

--- view.py
+++ view.py
@@ -1,6 +1,51 @@
 import tkinter as tk
-from PIL import Image, ImageTk
+from tkinter import messagebox
 
-class View:
-    def __init__(self):
-        pass
+class Page(tk.Toplevel):
+    def __init__(self, controller, title):
+        super().__init__(controller.root)
+        self.controller = controller
+        self.title(title)
+        self.geometry("500x500")
+
+    def show(self):
+        self.controller.hide_all()
+        self.deiconify()
+
+    def hide(self):
+        self.withdraw()
+
+    def close(self):
+        self.destroy()
+
+class Login(Page):
+    def __init__(self, controller):
+        super().__init__(controller, "Login")
+        self.label_email = tk.Label(self, text="Email:")
+        self.label_pw = tk.Label(self, text="Password:")
+        self.entry_email = tk.Entry(self)
+        self.entry_pw = tk.Entry(self, show='*')
+        self.btn_login = tk.Button(self, text="Login", command=self.login)
+
+        self.label_email.pack()
+        self.entry_email.pack()
+        self.label_email.pack()
+        self.entry_pw.pack()
+        self.btn_login.pack()
+    
+    def login(self):
+        email = self.entry_email.get()
+        pw = self.entry_pw.get()
+        self.controller.login(email, pw)
+
+class Doctor(Page):
+    def __init__(self, controller):
+        super().__init__(controller, "Doctor Portal")
+
+class Patient(Page):
+    def __init__(self, controller):
+        super().__init__(controller, "Patient Portal")
+
+class Admin(Page):
+    def __init__(self, controller):
+        super().__init__(controller, "Admin Portal")
